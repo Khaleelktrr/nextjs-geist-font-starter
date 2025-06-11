277901a7bd910953ccd1f4630d05a8097c85c60f
Khaleelktrr <khaleelkattathar@gmail.com>
2025-06-11 18:59:14 +0530

    Add .next/static/chunks/src_app_book_[id]_page_tsx_55213054._.js 1749648546858

---
+++ .next/static/chunks/src_app_book_[id]_page_tsx_55213054._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_5207fb7e._.js",
+  "static/chunks/src_bf7bb17f._.js"
+],
+    source: "dynamic"
+});
